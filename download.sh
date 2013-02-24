97c225284135d6571f257c618715728ddf582589
Jorma <jorma@localhost.(none)>
2013-02-23 19:50:54 -1000

    gathering initial pieces

---
+++ download.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+#
+
+cd /Users/jorma/Code/maui_bigwave_alerts/images
+rm /Users/jorma/Code/maui_bigwave_alerts/images/*.png
+
+/opt/local/bin/wget http://www.stormsurfing.com/stormuser2/images/grib/hi_comp_1.png
+
